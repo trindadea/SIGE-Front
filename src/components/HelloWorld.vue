81bba5a91ae5b556a83fc404fcfbafbb1a0340d1
Guilherme Augusto <guilherme.francais@gmail.com>
2019-06-03 10:58:55 -0300

    Creates a simple graph with 3 lines

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -7,18 +7,6 @@
           :options="chartOptions"
           :series="series"/>
       </b-col>
-      <b-col>
-        <apexcharts
-          width="100%"
-          :options="chartOptions2"
-          :series="series"/>
-      </b-col>
-      <b-col>
-        <apexcharts
-          width="100%"
-          :options="chartOptions3"
-          :series="series"/>
-      </b-col>
     </b-row>
     <!-- {{chartOptions.xaxis.categories}} -->
     <hr>
@@ -31,166 +19,102 @@ import VueApexCharts from 'vue-apexcharts'
 import axios from 'axios'
 
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
   components: {
     apexcharts: VueApexCharts
   },
-  data () {
+  data() {
     return {
-      ready: false,
-      api: {
-        categories: [],
-        dataset: []
-      },
-      a: {}
+      object: {}
     }
   },
-
   computed: {
-    chartOptions () {
+    chartOptions() {
       return {
         chart: {
-          id: 'vuechart-example',
-          type: 'line',
-          zoom: {
-            enabled: true,
-            // type: 'x',
-            // zoomedArea: {
-            //   fill: {
-            //     color: '#90CAF9',
-            //     opacity: 0.4
-            //   },
-            //   stroke: {
-            //     color: '#0D47A1',
-            //     opacity: 0.4,
-            //     width: 1
-            //   }
-            // }
+          stacked: false
+        },
+        stroke: {
+          width: [2, 2, 2],
+          curve: "smooth"
+        },
+        plotOptions: {
+          bar: {
+            columnWidth: "50%"
           }
+        },
+        fill: {
+          opacity: [0.85, 0.25, 1],
+          gradient: {
+            inverseColors: false,
+            shade: "light",
+            type: "vertical",
+            opacityFrom: 0.85,
+            opacityTo: 0.55,
+            stops: [0, 100, 100, 100]
+          }
+        },
+        labels: this.createGraphData(this.object, "BRL")[0],
+        markers: {
+          size: 0
         },
         xaxis: {
-          categories: this.createDates(this.a),
-          labels: {
-            type: "datetime",
-            show: false
-          }
+          type: "datetime",
+          show: false
         },
         yaxis: {
+          title: {
+            text: "Voltage"
+          },
           min: 1,
           max: 4.5,
           tickAmount: 6,
           labels: {
             formatter: this.labelFormatter
-          }
-        },
-        dataLabels: {
-          enabled: false,
+          },
+          min: 0
         }
       }
     },
-    chartOptions2 () {
-      return {
-        chart: {
-          id: 'vuechart-example',
-          type: 'area',
-          zoom: {
-            enabled: true,
-            type: 'x',
-            zoomedArea: {
-              fill: {
-                color: '#90CAF9',
-                opacity: 0.4
-              },
-              stroke: {
-                color: '#0D47A1',
-                opacity: 0.4,
-                width: 1
-              }
-            }
-          }
+    series() {
+      return [
+        {
+          name: "BRL",
+          type: "line",
+          data: this.createGraphData(this.object, "BRL")[1],
+          labels: []
         },
-        xaxis: {
-          categories: this.createDates(this.a),
-          labels: {
-            type: "datetime",
-            show: false
-          }
+        {
+          name: "MYR",
+          type: "line",
+          data: this.createGraphData(this.object, "MYR")[1],
+          labels: []
         },
-        yaxis: {
-          min: 1,
-          max: 4.5,
-          tickAmount: 6,
-          labels: {
-            formatter: this.labelFormatter
-          }
-        },
-        dataLabels: {
-          enabled: false,
+        {
+          name: "SGD",
+          type: "line",
+          data: this.createGraphData(this.object, "SGD")[1],
+          labels: []
         }
-      }
-    },
-    chartOptions3 () {
-      return {
-        chart: {
-          id: 'vuechart-example',
-          type: 'bar',
-          zoom: {
-            enabled: true,
-            type: 'x',
-            zoomedArea: {
-              fill: {
-                color: '#90CAF9',
-                opacity: 0.4
-              },
-              stroke: {
-                color: '#0D47A1',
-                opacity: 0.4,
-                width: 1
-              }
-            }
-          }
-        },
-        xaxis: {
-          categories: this.averageDataPerYear(this.a)[0],
-          labels: {
-            type: "datetime",
-            show: false
-          }
-        },
-        yaxis: {
-          min: 1,
-          max: 4.5,
-          tickAmount: 6,
-          labels: {
-            formatter: this.labelFormatter
-          }
-        },
-        dataLabels: {
-          enabled: false,
-        }
-      }
-    },
-
-    series () {
-      return [{data: this.averageDataPerYear(this.a)[1]}]
+      ]
     }
   },
-
   methods: {
     updateChartAxis () {
-      let start = '2010-01-01'
+      let start = '2018-01-01'
       let end = '2019-01-01'
       let a = []
       let b = []
       axios
         .get(`https://api.exchangeratesapi.io/history?start_at=${start}&end_at=${end}&base=USD`)
         .then((res) => {
-          this.a = res.data.rates
+          this.object = res.data.rates
         })
     },
     createDates (dataset) {
       let keys = []
       for (let k in dataset) keys.push(k)
+
       keys.sort((a,b) => {
         a = a.split("-").join("")
         b = b.split("-").join("")
@@ -199,13 +123,28 @@ export default {
 
       return keys
     },
-    getData (dataset) {
-      const keys = this.createDates(dataset)
-      let rates = []
-      keys.forEach((k, i) => {
-        rates.push(dataset[k].BRL)
+    createGraphData (dataset, coin) {
+      let unformatted_keys = this.createDates(dataset)
+      let formatted_keys = []
+      let value
+
+      // console.log(unformatted_keys)
+
+      for(let k = 0; k < unformatted_keys.length; k++) {
+        value = unformatted_keys[k].split("-")
+        value = value[1] + "/" + value[2] + "/" + value[0]
+        formatted_keys.push(value)
+      }
+
+      console.log(formatted_keys)
+
+      let values = []
+
+      unformatted_keys.forEach((k, i) => {
+        values.push(dataset[k][coin])
       })
-      return rates
+
+      return [formatted_keys, values]
     },
     labelFormatter (value) {
       return value.toFixed(2)
@@ -235,12 +174,13 @@ export default {
           }
         }
       })
-      return [date_year, rates_average]
+
+    return [date_year, rates_average]
     }
   },
   beforeMount () {
     this.updateChartAxis()
-  },
+  }
 }
 </script>
 
