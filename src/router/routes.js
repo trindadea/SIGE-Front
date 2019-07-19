0da9b4d053fc1c46b1ea7d356fdb8ea00b2e873b
Andre Filho <andre.filho001@outlook.com>
2019-07-19 11:04:44 -0300

    CHR: added initial routes

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,15 +1,51 @@
 
 const routes = [
   {
-    path: '/a',
+    path: '/',
     component: () => import('layouts/MyLayout.vue'),
     children: [
       { path: '', component: () => import('pages/Index.vue') }
     ]
   },
   {
-    path: '/',
-    component: () => import('components/form.vue')
+    path: '/transductors',
+    component: () => import('components/transductors/Transductors.vue'),
+    children: [
+      { path: '/:id', component: () => import('components/transductors/Show.vue'), props: true },
+      { path: '/:id/edit', component: () => import('components/transductors/Form.vue'), props: true },
+      { path: '/new', component: () => import('components/transductors/Form.vue') }
+    ]
+  },
+  {
+    path: '/servers',
+    component: () => import('components/slaves/Slaves.vue'),
+    children: [
+      { path: '/:id', component: () => import('components/slaves/Show.vue'), props: true },
+      { path: '/:id/edit', component: () => import('components/slaves/Form.vue'), props: true },
+      { path: '/new', component: () => import('components/slaves/Form.vue') }
+    ]
+  },
+  {
+    path: '/buildings',
+    component: () => import('components/buildings/Buildings.vue'),
+    children: [
+      { path: '/:id', component: () => import('components/buildings/Show.vue'), props: true },
+      { path: '/:id/edit', component: () => import('components/buildings/Form.vue'), props: true },
+      { path: '/new', component: () => import('components/buildings/Form.vue') }
+    ]
+  },
+  {
+    path: '/campi',
+    component: () => import('components/campi/Campi.vue'),
+    children: [
+      { path: '/:id', component: () => import('components/campi/Show.vue'), props: true },
+      { path: '/:id/edit', component: () => import('components/campi/Form.vue'), props: true },
+      { path: '/new', component: () => import('components/campi/Form.vue') }
+    ]
+  },
+  {
+    path: '/dashboard',
+    component: () => import('components/dashboard/Dashboard.vue')
   }
 ]
 
