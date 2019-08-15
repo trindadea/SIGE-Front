567897c3aa533b20d40eb6e9449e3e4f9f859d81
Andre Filho <andre.filho001@outlook.com>
2019-08-15 11:45:05 -0300

    ADD: creates the dashboardsummarystatuscard component for dashboarding purposes

--- src/components/dashboard/StatusDashboard.vue
+++ src/components/dashboard/StatusDashboard.vue
@@ -1,13 +1,62 @@
 <template>
   <q-page>
-    <div class="row">
-      Status screen will be here
-    </div>
-</q-page>
+    <section
+      id="summary-status"
+      class="q-py-md">
+      <h4 class="text-grey-7 q-my-xs q-mx-md">
+        Resumo do sistema
+      </h4>
+      <q-separator/>
+      <div
+        id="cards"
+        class="row">
+        <d-status-card
+          class="col-sm-6 col-md-3"
+          :maxValue="20"
+          :value="15"
+          title="Transdutores"/>
+        <!-- <q-banner
+          rounded
+          class=""
+          v-for="item in items"
+          :key="item.id">
+          {{ item }}
+        </q-banner> -->
+      </div>
+    </section>
+    <section id="transductor-status">
+      <h4 class="text-grey-7 q-my-xs q-mx-md">
+        Estado dos transdutores
+      </h4>
+      <q-separator/>
+      <div>
+
+      </div>
+    </section>
+    <section id="slave-status">
+      <h4 class="text-grey-7 q-my-xs q-mx-md">
+        Estado dos servidores escravos
+      </h4>
+      <q-separator/>
+      <div>
+
+      </div>
+    </section>
+    <!-- <section id="database-status">
+      <div>
+
+      </div>
+    </section> -->
+  </q-page>
 </template>
 
 <script>
+import DashboardSummaryStatusCard from 'components/dashboard/DashboardSummaryStatusCard.vue'
+
 export default {
+  components: {
+    'd-status-card': DashboardSummaryStatusCard
+  }
 
 }
 </script>
