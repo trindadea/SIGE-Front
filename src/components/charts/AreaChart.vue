2d25563f3e56b1df26c6505a731a8cc573b4022f
Andre Filho <andre.filho001@outlook.com>
2019-10-03 18:31:41 -0300

    ADD: base file for barcharts

--- src/components/charts/AreaChart.vue
+++ src/components/charts/AreaChart.vue
@@ -1,94 +1,312 @@
 <template>
-  <apexcharts
-    id="chart"
-    type="area"
-    :options="chartOptions"
-    :series="chartSeries"/>
+  <q-page>
+    <div class="row q-pa-sm">
+        <q-select
+          class="col q-ma-sm"
+          label="Transdutor"
+          outlined
+          :options="this.transductorList"
+          v-model="selectedTransductor"
+          @input="updateChart()"/>
+        <q-select
+          class="col q-ma-sm"
+          label="Período"
+          outlined
+          v-model="selectedPeriod"
+          :options="['Hoje', 'Últimos 7 dias', 'Últimos 30 dias']"
+          @input="updateChart()"/>
+    </div>
+    <q-separator/>
+    <div
+      v-if="this.selectedTransductor !== ''">
+      <apexcharts
+      id="chart"
+      type="area"
+      :options="chartOptions"
+      :series="series"/>
+    </div>
+    <no-data-placeholder v-else/>
+  </q-page>
 </template>
 
 <script>
-// width="50%"
 import VueApexCharts from 'vue-apexcharts'
+import NoDataPlaceholder from './NoDataPlaceholder.vue'
+import moment from 'moment'
+import axios from 'axios'
 
 export default {
   components: {
-    'apexcharts': VueApexCharts
+    'apexcharts': VueApexCharts,
+    'no-data-placeholder': NoDataPlaceholder
   },
 
   props: [
-    'series',
-    'xaxis',
-    'yaxis',
-    'yannotations',
-    'xannotations',
     'title',
-    'colors'
+    'url',
+    'graphic_type'
   ],
 
   data () {
     return {
-      chartSeries: [],
-      chartOptions: {
-        annotations: {
-          xaxis: [],
-          yaxis: []
+      dates: [],
+      fase_a: [],
+      fase_b: [],
+      fase_c: [],
+      measurements: [],
+      transductorList: [],
+      selectedCampus: '',
+      selectedTransductor: '',
+      selectedPeriod: 'Hoje',
+      periodsOptions: {}
+    }
+  },
+
+  computed: {
+    series () {
+      return [
+        {
+          name: 'Fase A',
+          data: this.fase_a
         },
+        {
+          name: 'Fase B',
+          data: this.fase_b
+        },
+        {
+          name: 'Fase C',
+          data: this.fase_c
+        }
+      ]
+    },
+    chartOptions () {
+      return {
         chart: {
-          shadow: {
-            enabled: true,
-            color: '#000',
-            top: 18,
-            left: 7,
-            blur: 10,
-            opacity: 1
-          },
-          toolbar: {
-            show: false
-          }
+          stacked: false
         },
-        colors: ['#3333ee', '#33ee33', '#ee3333'],
-        dataLabels: {
-          enabled: true
-        },
+
         stroke: {
+          width: [2, 2, 2],
           curve: 'smooth'
         },
-        title: {
-          text: '',
-          align: 'left'
+
+        plotOptions: {
+          bar: {
+            columnWidth: '50%'
+          }
         },
+
+        fill: {
+          opacity: [0.85, 0.25, 1],
+          gradient: {
+            inverseColors: false,
+            shade: 'light',
+            type: 'vertical',
+            opacityFrom: 0.85,
+            opacityTo: 0.55,
+            stops: [0, 100, 100, 100]
+          }
+        },
+
+        labels: this.dates,
+
+        dataLabels: {
+          enabled: false
+        },
+
+        markers: {
+          size: 0
+        },
+
+        xaxis: {
+          type: 'datetime',
+          show: false
+        },
+
+        yaxis: {
+          title: {
+            text: 'frequency'
+          },
+          min: Math.min(...this.series[0].data) - 20,
+          max: Math.max(...this.series[0].data) + 20,
+          tickAmount: 5
+        },
+
         grid: {
           borderColor: '#e7e7e7',
           row: {
-            colors: ['#f3f3f3', 'transparent'], // takes an array which will be repeated on columns
+            colors: ['#f3f3f3', 'transparent'],
             opacity: 0.5
           }
-        },
-        markers: {
-          size: 5
-        },
-        xaxis: {},
-        yaxis: {},
-        legend: {
-          position: 'top',
-          horizontalAlign: 'right',
-          floating: true,
-          offsetY: -25,
-          offsetX: -5
         }
       }
     }
   },
 
+  methods: {
+    updateChart () {
+      let periods = this.periodsOptions[this.selectedPeriod]
+      let startDate = periods[0]
+      let endDate = periods[1]
+      let limit = periods[2]
+
+      if (this.selectedTransductor !== undefined) {
+        axios
+          .get(`http://127.0.0.1:8000/graph/minutely_${this.url}/?limit=${limit}&serial_number=${this.selectedTransductor}&start_date=${startDate}&end_date=${endDate}`)
+          .then((res) => {
+            const measurements = res.data.results
+            console.log(measurements)
+            this.buildGraphInformation(measurements)
+          })
+          .catch((err) => console.log(err))
+      }
+    },
+
+    getTodayInterval () {
+      let now = new Date()
+      let oneDayAgo = new Date()
+
+      oneDayAgo.setDate(oneDayAgo.getDate() - 1)
+
+      let startDate = moment(oneDayAgo).format('YYYY-MM-DD h:mm')
+      let endDate = moment(now).format('YYYY-MM-DD h:mm')
+
+      return [startDate, endDate, 1440]
+    },
+
+    getLastWeek () {
+      let now = new Date()
+      let oneWeekAgo = new Date()
+
+      oneWeekAgo.setDate(oneWeekAgo.getDate() - 7)
+
+      let startDate = moment(oneWeekAgo).format('YYYY-MM-DD h:mm')
+      let endDate = moment(now).format('YYYY-MM-DD h:mm')
+
+      return [startDate, endDate, 10080]
+    },
+
+    getLastMonth () {
+      let now = new Date()
+      let oneMonthAgo = new Date()
+
+      oneMonthAgo.setMonth(oneMonthAgo.getMonth() - 1)
+
+      let startDate = moment(oneMonthAgo).format('YYYY-MM-DD h:mm')
+      let endDate = moment(now).format('YYYY-MM-DD h:mm')
+
+      return [startDate, endDate, 43200]
+    },
+
+    formattedDate (date) {
+      let dateValue
+      let timeValue
+      let result = date.split('T')
+
+      dateValue = result[0].split('-')
+      dateValue = dateValue[1] + '/' + dateValue[2] + '/' + dateValue[0]
+      timeValue = result[1]
+
+      return dateValue + ' ' + timeValue
+    },
+
+    buildGraphInformation (measurements) {
+      if (this.graphic_type === '1') {
+        let date
+
+        let oneFaseMeasurement
+        let formattedDates = []
+
+        let measurementList = []
+
+        for (let measurement of measurements) {
+          date = measurement['collection_date']
+          date = this.formattedDate(date)
+
+          oneFaseMeasurement = measurement['measurement']
+
+          formattedDates.push(date)
+
+          measurementList.push(oneFaseMeasurement)
+        }
+
+        this.setOneFaseInformations(measurementList, formattedDates)
+      } else {
+        let date
+
+        let faseA
+        let faseB
+        let faseC
+
+        let formattedDates = []
+
+        let faseAList = []
+        let faseBList = []
+        let faseCList = []
+
+        for (let measurement of measurements) {
+          date = measurement['collection_date']
+          date = this.formattedDate(date)
+
+          faseA = measurement['phase_a']
+          faseB = measurement['phase_b']
+          faseC = measurement['phase_c']
+
+          formattedDates.push(date)
+
+          faseAList.push(faseA)
+          faseBList.push(faseB)
+          faseCList.push(faseC)
+        }
+
+        this.setThreeFaseInformations(faseAList, faseBList, faseCList, formattedDates)
+      }
+    },
+
+    setOneFaseInformations (measurementList, formattedDates) {
+      this.fase_a = measurementList
+      this.dates = formattedDates
+    },
+
+    setThreeFaseInformations (faseAList, faseBList, faseCList, formattedDates) {
+      this.fase_a = faseAList
+      this.fase_b = faseBList
+      this.fase_c = faseCList
+      this.dates = formattedDates
+    },
+
+    setTransductorList (transductorList) {
+      this.transductorList = transductorList
+    },
+
+    getTransductors () {
+      axios
+        .get(`http://0.0.0.0:8000/energy_transductors`)
+        .then((res) => {
+          const transductors = res.data.results
+
+          let transductorsList = []
+
+          for (let transductor of transductors) {
+            transductorsList.push(transductor['serial_number'])
+          }
+
+          transductorsList.sort()
+
+          this.setTransductorList(transductorsList)
+        })
+        .catch((err) => {
+          console.log(err)
+        })
+    }
+  },
+
   beforeMount () {
-    // this.chartOptions = this.options
-    this.chartSeries = this.series
-    this.chartOptions.yaxis = this.yaxis
-    this.chartOptions.xaxis = this.xaxis
-    this.chartOptions.colors = this.colors
-    this.chartOptions.title.text = this.title
-    this.chartOptions.annotations.xaxis = this.xannotations
-    this.chartOptions.annotations.yaxis = this.yannotations
+    this.getTransductors()
+
+    this.periodsOptions['Hoje'] = this.getTodayInterval()
+    this.periodsOptions['Últimos 7 dias'] = this.getLastWeek()
+    this.periodsOptions['Últimos 30 dias'] = this.getLastMonth()
   }
 }
 </script>
