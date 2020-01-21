b435a0701029a9ae1f00ae72d7b0001701f01ad4
Andre Filho <andre.filho001@outlook.com>
2020-01-21 15:32:04 -0300

    ADD: vuex pkg; route to dashboard (/); basefile for dashboard

---
+++ src/components/dashboard/DashboardBase.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <dash-top-bar/>
+    <dash-campus-bar/>
+    <dash-general-event-bar/>
+  </div>
+</template>
+
+<script>
+import DashTopBar from 'components/dashboard/DashTopBar'
+import DashCampusBar from 'components/dashboard/DashCampusBar'
+import DashGeneralEventBar from 'components/dashboard/DashGeneralEventBar'
+
+export default {
+  name: 'DashboardBase',
+  components: {
+    DashTopBar,
+    DashCampusBar,
+    DashGeneralEventBar
+  },
+
+  data () {
+    return {}
+  },
+
+  methods: {
+
+  },
+
+  computed: {
+
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/router/routes.js
+++ src/router/routes.js
@@ -1,171 +1,175 @@
 
 const routes = [
   {
-    path: '/presentation',
-    component: () => import('components/presentationDashboard/PresentationDashboard.vue'),
-    children: [
-      {
-        path: '',
-        component: () => import('components/presentationDashboard/Page1.vue')
-      },
-      {
-        name: 'presentation_detail',
-        path: ':transductor_id/detail',
-        props: true,
-        component: () => import('components/presentationDashboard/Page2.vue')
-      }
-    ]
-  },
-  {
-    path: '/transductors',
-    component: () => import('components/transductors/TransductorsBase.vue'),
-    children: [
-      {
-        path: '',
-        component: () => import('components/transductors/Transductors.vue')
-      },
-      {
-        path: 'new',
-        component: () => import('components/transductors/Form.vue')
-      },
-      {
-        path: ':id',
-        component: () => import('components/transductors/Show.vue'),
-        props: true
-      },
-      {
-        path: ':id/edit',
-        component: () => import('components/transductors/Form.vue'),
-        props: true
-      }
-    ]
-  },
-  {
-    path: '/servers',
-    component: () => import('components/slaves/SlavesBase.vue'),
-    children: [
-      {
-        path: '',
-        component: () => import('components/slaves/Slaves.vue')
-      },
-      {
-        path: 'new',
-        component: () => import('components/slaves/Form.vue')
-      },
-      {
-        path: ':id',
-        component: () => import('components/slaves/Show.vue'),
-        props: true
-      },
-      {
-        path: ':id/edit',
-        component: () => import('components/slaves/Form.vue'),
-        props: true
-      }
-    ]
-  },
-  {
-    path: '/buildings',
-    component: () => import('components/buildings/BuildingsBase.vue'),
-    children: [
-      {
-        path: '',
-        component: () => import('components/buildings/Buildings.vue')
-      },
-      {
-        path: 'new',
-        component: () => import('components/buildings/Form.vue')
-      },
-      {
-        path: ':id',
-        component: () => import('components/buildings/Show.vue'),
-        props: true
-      },
-      {
-        path: ':id/edit',
-        component: () => import('components/buildings/Form.vue'),
-        props: true
-      }
-    ]
-  },
-  {
-    path: '/campi',
-    component: () => import('components/campi/CampiBase.vue'),
-    children: [
-      {
-        path: '',
-        component: () => import('components/campi/Campi.vue')
-      },
-      {
-        path: 'new',
-        component: () => import('components/campi/Form.vue')
-      },
-      {
-        path: ':id',
-        component: () => import('components/campi/Show.vue'),
-        props: true
-      },
-      {
-        path: ':id/edit',
-        component: () => import('components/campi/Form.vue'),
-        props: true
-      }
-    ]
-  },
-  {
-    path: '/transductor_models',
-    component: () => import('components/transductorModel/TransductorModelBase.vue'),
-    children: [
-      {
-        path: '',
-        component: () => import('components/transductorModel/TransductorModels.vue')
-      },
-      {
-        path: 'new',
-        component: () => import('components/transductorModel/Form.vue')
-      },
-      {
-        path: ':id',
-        component: () => import('components/transductorModel/Show.vue'),
-        props: true
-      },
-      {
-        path: ':id/edit',
-        component: () => import('components/transductorModel/Form.vue'),
-        props: true
-      }
-    ]
-  },
-  {
-    path: '/dashboard',
-    component: () => import('components/oldDashboard/Dashboard.vue'),
-    children: [
-      {
-        path: '',
-        component: () => import('components/oldDashboard/StatusDashboard.vue')
-      },
-      {
-        path: 'tension',
-        component: () => import('components/oldDashboard/TensionDashboard.vue')
-      },
-      {
-        path: 'current',
-        component: () => import('components/oldDashboard/CurrentDashboard.vue')
-      },
-      {
-        path: 'frequency',
-        component: () => import('components/oldDashboard/FrequencyDashboard.vue')
-      },
-      {
-        path: 'total_apparent_power',
-        component: () => import('components/oldDashboard/TotalApparentPowerDashboard.vue')
-      },
-      {
-        path: 'consumption',
-        component: () => import('components/oldDashboard/ConsumptionDashboard.vue')
-      }
-    ]
+    path: '/',
+    component: () => import('components/dashboard/DashboardBase.vue')
   }
+  // {
+  //   path: '/presentation',
+  //   component: () => import('components/presentationDashboard/PresentationDashboard.vue'),
+  //   children: [
+  //     {
+  //       path: '',
+  //       component: () => import('components/presentationDashboard/Page1.vue')
+  //     },
+  //     {
+  //       name: 'presentation_detail',
+  //       path: ':transductor_id/detail',
+  //       props: true,
+  //       component: () => import('components/presentationDashboard/Page2.vue')
+  //     }
+  //   ]
+  // },
+  // {
+  //   path: '/transductors',
+  //   component: () => import('components/transductors/TransductorsBase.vue'),
+  //   children: [
+  //     {
+  //       path: '',
+  //       component: () => import('components/transductors/Transductors.vue')
+  //     },
+  //     {
+  //       path: 'new',
+  //       component: () => import('components/transductors/Form.vue')
+  //     },
+  //     {
+  //       path: ':id',
+  //       component: () => import('components/transductors/Show.vue'),
+  //       props: true
+  //     },
+  //     {
+  //       path: ':id/edit',
+  //       component: () => import('components/transductors/Form.vue'),
+  //       props: true
+  //     }
+  //   ]
+  // },
+  // {
+  //   path: '/servers',
+  //   component: () => import('components/slaves/SlavesBase.vue'),
+  //   children: [
+  //     {
+  //       path: '',
+  //       component: () => import('components/slaves/Slaves.vue')
+  //     },
+  //     {
+  //       path: 'new',
+  //       component: () => import('components/slaves/Form.vue')
+  //     },
+  //     {
+  //       path: ':id',
+  //       component: () => import('components/slaves/Show.vue'),
+  //       props: true
+  //     },
+  //     {
+  //       path: ':id/edit',
+  //       component: () => import('components/slaves/Form.vue'),
+  //       props: true
+  //     }
+  //   ]
+  // },
+  // {
+  //   path: '/buildings',
+  //   component: () => import('components/buildings/BuildingsBase.vue'),
+  //   children: [
+  //     {
+  //       path: '',
+  //       component: () => import('components/buildings/Buildings.vue')
+  //     },
+  //     {
+  //       path: 'new',
+  //       component: () => import('components/buildings/Form.vue')
+  //     },
+  //     {
+  //       path: ':id',
+  //       component: () => import('components/buildings/Show.vue'),
+  //       props: true
+  //     },
+  //     {
+  //       path: ':id/edit',
+  //       component: () => import('components/buildings/Form.vue'),
+  //       props: true
+  //     }
+  //   ]
+  // },
+  // {
+  //   path: '/campi',
+  //   component: () => import('components/campi/CampiBase.vue'),
+  //   children: [
+  //     {
+  //       path: '',
+  //       component: () => import('components/campi/Campi.vue')
+  //     },
+  //     {
+  //       path: 'new',
+  //       component: () => import('components/campi/Form.vue')
+  //     },
+  //     {
+  //       path: ':id',
+  //       component: () => import('components/campi/Show.vue'),
+  //       props: true
+  //     },
+  //     {
+  //       path: ':id/edit',
+  //       component: () => import('components/campi/Form.vue'),
+  //       props: true
+  //     }
+  //   ]
+  // },
+  // {
+  //   path: '/transductor_models',
+  //   component: () => import('components/transductorModel/TransductorModelBase.vue'),
+  //   children: [
+  //     {
+  //       path: '',
+  //       component: () => import('components/transductorModel/TransductorModels.vue')
+  //     },
+  //     {
+  //       path: 'new',
+  //       component: () => import('components/transductorModel/Form.vue')
+  //     },
+  //     {
+  //       path: ':id',
+  //       component: () => import('components/transductorModel/Show.vue'),
+  //       props: true
+  //     },
+  //     {
+  //       path: ':id/edit',
+  //       component: () => import('components/transductorModel/Form.vue'),
+  //       props: true
+  //     }
+  //   ]
+  // }
+  // {
+  //   path: '/dashboard',
+  //   component: () => import('components/dashboard/oldDashboard/Dashboard.vue'),
+  //   children: [
+  //     {
+  //       path: '',
+  //       component: () => import('components/dashboard/oldDashboard/StatusDashboard.vue')
+  //     },
+  //     {
+  //       path: 'tension',
+  //       component: () => import('components/dashboard/oldDashboard/TensionDashboard.vue')
+  //     },
+  //     {
+  //       path: 'current',
+  //       component: () => import('components/dashboard/oldDashboard/CurrentDashboard.vue')
+  //     },
+  //     {
+  //       path: 'frequency',
+  //       component: () => import('components/dashboard/oldDashboard/FrequencyDashboard.vue')
+  //     },
+  //     {
+  //       path: 'total_apparent_power',
+  //       component: () => import('components/dashboard/oldDashboard/TotalApparentPowerDashboard.vue')
+  //     },
+  //     {
+  //       path: 'consumption',
+  //       component: () => import('components/dashboard/oldDashboard/ConsumptionDashboard.vue')
+  //     }
+  //   ]
+  // }
 ]
 
 // Always leave this as last one
