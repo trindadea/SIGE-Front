fb96964d6a933b3f420a05a48fb61d0ca7462d21
Andre Filho <andre.filho001@outlook.com>
2020-01-22 15:26:09 -0300

    ADD: initial styling for top bar

--- src/components/dashboard/DashTopBar.vue
+++ src/components/dashboard/DashTopBar.vue
@@ -1,13 +1,41 @@
 <template>
-  <div></div>
+  <q-bar
+    dense
+    class="q-py-none q-px-sm row">
+    <div
+      class="col">
+      <img src="statics/transparents/proj_trans_s.png" height="30px">
+    </div>
+    <div
+      id="alerts"
+      class="col text-center">
+      {{ alerts !== [] ? alerts.length : 0 }} ocorrências em andamento
+    </div>
+    <div
+      id="datetime"
+      class="col text-right">
+      {{Date.now()}}
+    </div>
+  </q-bar>
 </template>
 
 <script>
 export default {
+  name: 'DashTopBar',
+
+  data () {
+    return {
+      alerts: []
+    }
+  }
 
 }
 </script>
 
 <style lang="scss" scoped>
+  .q-bar {
+    background-color: #22222f;
+    color: #f3f3f3;
+  }
 
 </style>
--- src/components/dashboard/DashboardBase.vue
+++ src/components/dashboard/DashboardBase.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="odin">
     <dash-top-bar/>
     <dash-campus-bar/>
     <dash-general-event-bar/>
@@ -34,5 +34,7 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+  .odin {
+    background-color: #22222f;
+  }
 </style>
