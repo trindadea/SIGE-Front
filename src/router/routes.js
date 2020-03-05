af245763a6613ca3b023c2aa574992c4e4a160e6
Alax Alves <alaxallves@gmail.com>
2020-03-05 18:04:51 -0300

    Remove unused files

--- src/router/routes.js
+++ src/router/routes.js
@@ -3,21 +3,6 @@ const routes = [
   {
     path: '/',
     component: () => import('components/presentationDashboard/PresentationDashboard.vue'),
-    children: [
-      {
-        path: '',
-        component: () => import('components/presentationDashboard/Page1.vue')
-      },
-      {
-        name: 'presentation_detail',
-        path: ':transductor_id/detail',
-        props: true,
-        component: () => import('components/presentationDashboard/Page2.vue')
-      },
-      {
-        path: '/about', component: () => import('pages/About.vue')
-      }
-    ]
   },
   {
     path: '/transductors',
