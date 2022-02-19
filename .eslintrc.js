01a12489379adbaa5b1f2090beb92894d40123c1
Danillo Souza <danillosouza1704@gmail.com>
2022-02-19 19:44:13 -0300

    Organizado .eslint-rc, preettier já está funcional

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,4 @@
 module.exports = {
-  // https://eslint.org/docs/user-guide/configuring#configuration-cascading-and-hierarchy
-  // This option interrupts the configuration hierarchy at this file
-  // Remove this if you have an higher level ESLint config file (it usually happens into a monorepos)
-  root: true,
 
   parserOptions: {
     parser: 'babel-eslint',
@@ -11,29 +7,17 @@ module.exports = {
   },
 
   env: {
+    node: true,
     browser: true
   },
 
-  // Rules order is important, please avoid shuffling them
   extends: [
-    // Base ESLint recommended rules
-    // 'eslint:recommended',
-
-
-    // Uncomment any of the lines below to choose desired strictness,
-    // but leave only one uncommented!
-    // See https://eslint.vuejs.org/rules/#available-rules
-    'plugin:vue/essential', // Priority A: Essential (Error Prevention)
-    // 'plugin:vue/strongly-recommended', // Priority B: Strongly Recommended (Improving Readability)
-    // 'plugin:vue/recommended', // Priority C: Recommended (Minimizing Arbitrary Choices and Cognitive Overhead)
-    "plugin:prettier/recommended",
-    'standard'
-
+    'plugin:vue/essential',
+    'eslint:recommended',
+    // "plugin:vue/vue3-recommended",
+    "prettier"
   ],
-
   plugins: [
-    // https://eslint.vuejs.org/user-guide/#why-doesn-t-it-work-on-vue-file
-    // required to lint *.vue files
     'vue',
     'prettier'
   ],
@@ -47,14 +31,10 @@ module.exports = {
     'chrome': true
   },
 
-  // add your custom rules here
   rules: {
-    // allow async-await
     'generator-star-spacing': 'off',
-    // allow paren-less arrow functions
     'arrow-parens': 'off',
     'one-var': 'off',
-
     'import/first': 'off',
     'import/named': 'error',
     'import/namespace': 'error',
@@ -62,11 +42,10 @@ module.exports = {
     'import/export': 'error',
     'import/extensions': 'off',
     'import/no-unresolved': 'off',
+    'vue/no-unused-vars': 'error',
     'import/no-extraneous-dependencies': 'off',
     'prefer-promise-reject-errors': 'off',
-    "prettier/prettier": ["error"],
-
-    // allow debugger during development only
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    "vue/require-default-prop": "off",
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
   }
 }
