8a344ef756a924c579e4046335f192693323202a
Danillo Souza <danillosouza1704@gmail.com>
2022-02-22 22:20:39 -0300

    Adicionando storybook configs

---
+++ .storybook/main.js
@@ -0,0 +1,14 @@
+const webpackFinal = require('../webpack-config.js');
+
+module.exports = {
+  "stories": [
+    "../src/**/*.stories.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials"
+  ],
+  "framework": "@storybook/vue",
+  // webpackFinal,
+}
