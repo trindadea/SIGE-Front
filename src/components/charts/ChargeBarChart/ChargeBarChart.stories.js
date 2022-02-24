d798cfbc99cf3e433cc4600997181a28673d3d37
Nilo Mendonça <nilojrmendonca@gmail.com>
2022-02-24 13:03:35 -0300

    #53 - Pipeline error fix

--- src/components/charts/ChargeBarChart/ChargeBarChart.stories.js
+++ src/components/charts/ChargeBarChart/ChargeBarChart.stories.js
@@ -15,9 +15,13 @@ const Template = (args, { argTypes }) => ({
 export const ChargeBarChartComponent = Template.bind({})
 ChargeBarChartComponent.args = {
 <<<<<<< HEAD
+<<<<<<< HEAD
   selectedCampus: { id: 2 },
 =======
   selectedCampus: {id: 2},
 >>>>>>> 763afcc (#53 - Addition of the 'ChargeBarChart' component storybook)
+=======
+  selectedCampus: { id: 2 },
+>>>>>>> 8c68396 (#53 - Pipeline error fix)
   selectedTransductor: undefined
 }
