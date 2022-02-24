0d497ea7e56807ec276c35d0169dd5baeaa0abe6
luucas-melo <lucasmelodos322@gmail.com>
2022-02-24 16:17:38 -0300

    Add ConsumptionFilter story

--- .storybook/main.js
+++ .storybook/main.js
@@ -10,5 +10,17 @@ module.exports = {
     "@storybook/addon-essentials"
   ],
   "framework": "@storybook/vue",
-  // webpackFinal,
+  webpackFinal: async (config) => {
+
+    // add SCSS support for CSS Modules
+    config.module.rules.push({
+        test: /\.scss$/,
+        use: [
+            require.resolve("style-loader"),
+            require.resolve("css-loader"),
+        ],
+    });
+
+    return config;
+  }
 }
