882884fa215cec856ea62e480f54af8b79d2bd1c
danillo <danillosouza1704@gmail.com>
2022-03-19 15:37:07 -0300

    Adicionada mudança da main

--- .storybook/main.js
+++ .storybook/main.js
@@ -11,17 +11,5 @@ module.exports = {
   ],
   "staticDirs": [{from: '../src/statics', to:'/statics'}],
   "framework": "@storybook/vue",
-  webpackFinal: async (config) => {
-
-    // add SCSS support for CSS Modules
-    config.module.rules.push({
-        test: /\.scss$/,
-        use: [
-            require.resolve("style-loader"),
-            require.resolve("css-loader"),
-        ],
-    });
-
-    return config;
-  }
+  webpackFinal
 }
