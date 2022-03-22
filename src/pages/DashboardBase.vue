8c899129177ce5871d6d14b1bec3c8c30b4fbf48
Pedrok99 <pedrohlima20@gmail.com>
2022-03-22 14:15:00 -0300

    #67 - updating import path

--- src/pages/DashboardBase.vue
+++ src/pages/DashboardBase.vue
@@ -28,7 +28,7 @@
 import MASTER from '../services/masterApi/http-common'
 
 import DashBottomBar from 'components/dashboard/DashBottomBar'
-import DashCampusTab from 'components/dashboard/DashCampusTab'
+import DashCampusTab from 'components/dashboard/DashCampusTab/DashCampusTab'
 import DashGeneralEventBar from 'components/dashboard/DashGeneralEventBar'
 
 export default {
