81f4182483872fe75000674b6a31053c1980ce6e
luucas-melo <lucasmelodos322@gmail.com>
2022-03-23 01:21:03 -0300

    64-Add input tests

--- src/components/ConsumptionFilter/ConsumptionFilter.spec.js
+++ src/components/ConsumptionFilter/ConsumptionFilter.spec.js
@@ -17,22 +17,57 @@ localVue.use(Quasar, { components })
 
 const store = new Vuex.Store({})
 
-describe('ConsumptionFilter', () => {
-  const wrapper = mount(ConsumptionFilter, {
-    store,
-    localVue
-  })
+const ConsumptionFilterComponent = mount(ConsumptionFilter, {
+  store,
+  localVue,
+  data () {
+    return {
+      optionsCampus: ['Faculdade do  Gama', 'Darcy Ribeiro - Gleba A']
+    }
+  },
+  stubs: ['q-bar']
+})
 
+describe('ConsumptionFilter', () => {
   it('exists', () => {
-    expect(wrapper).toBeTruthy()
+    expect(ConsumptionFilterComponent).toBeTruthy()
   })
 
   it('clicks in button', async () => {
-    const button = wrapper.find('button')
+    const button = ConsumptionFilterComponent.find('button')
     expect(button.classes()).toContain('q-btn')
 
     await button.trigger('click')
 
     expect(button).toBeTruthy()
+  })
+
+  it('expect to have 4 inputs', () => {
+    const inputNumber = ConsumptionFilterComponent.findAll('.q-field__label ').length
+    expect(inputNumber).toBe(4)
+  })
+
+  it('expect first input to be Campus', () => {
+    const campusInput = ConsumptionFilterComponent.findAll('.q-field__label ').at(0)
+
+    expect(campusInput.text()).toBe('Campus')
+  })
+
+  it('expect second input to be Filtro', () => {
+    const campusInput = ConsumptionFilterComponent.findAll('.q-field__label ').at(1)
+
+    expect(campusInput.text()).toBe('Filtro')
+  })
+
+  it('expect second third input to contain Início', () => {
+    const campusInput = ConsumptionFilterComponent.findAll('.q-field__label ').at(2)
+
+    expect(campusInput.text()).toContain('Início')
+  })
+
+  it('expect second fourth input to contain Fim', () => {
+    const campusInput = ConsumptionFilterComponent.findAll('.q-field__label ').at(3)
+
+    expect(campusInput.text()).toContain('Fim')
   })
 })
